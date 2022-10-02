736700b02899fe85f7632f3233cfc152e63cb947
NMMcDonald <nicolasmmcdonald@gmail.com>
2022-10-01 23:30:00 -1000

    saving python file

---
+++ typingtest.py
@@ -0,0 +1,14 @@
+import curses
+from curses import wrapper
+
+def main(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Hello world")
+    stdscr.refresh()
+    stdscr.getkey()
+
+
+
+
+    
+wrapper(main)
